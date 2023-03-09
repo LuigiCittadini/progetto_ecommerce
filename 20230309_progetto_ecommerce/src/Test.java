3a28a89744e0d926ff24229f1dfda2c60d6619aa
itsrob00 <roberto.prats00gmail.com>
2023-03-09 16:29:54 +0100

    FILE JAR

--- 20230309_progetto_ecommerce/src/Test.java
+++ 20230309_progetto_ecommerce/src/Test.java
@@ -7,7 +7,7 @@ public class Test {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
-		Prodotto p = new Prodotto("AAA001", "Libro", 10, 3.90, "A01");
+		Prodotto p = new Prodotto("AAA011", "Libro", 10, 3.90, "A11");
 		GestoreController gc = new GestoreController();
 		gc.insert(p);
 
