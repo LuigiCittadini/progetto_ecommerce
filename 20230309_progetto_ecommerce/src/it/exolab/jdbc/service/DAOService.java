a65785647aa79643fb46d4fb51e3ef0d0b722736
Francesca <Francescadinisio@live.it>
2023-03-09 14:47:24 +0100

    classe service

---
+++ 20230309_progetto_ecommerce/src/it/exolab/jdbc/service/DAOService.java
@@ -0,0 +1,58 @@
+package it.exolab.jdbc.service;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DAOService {
+
+	private static DAOService instance = null;
+	private Connection connection = null;
+	
+	private DAOService() {}
+	
+	public static DAOService getInstance() {
+		if(instance == null) {
+			instance = new DAOService();
+		}
+		
+		return instance;
+	}
+	
+	private void closeConnection() throws SQLException {
+		
+		if(connection == null) {
+			return;
+		}
+		
+		connection.close();
+		connection = null;
+		
+	}
+	
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+		
+		if(connection != null) {
+			return connection;
+		}
+		
+		Class.forName("oracle.jdbc.driver.OracleDriver");  
+			  
+			//step2 create  the connection object  
+			
+			
+			
+			String host = "192.168.100.37";
+			String connectionString = "jdbc:oracle:thin:@" + host +":1521/pdb1";
+			
+			connection=DriverManager.getConnection(  
+					connectionString,"u12","u12");  
+					  
+			
+		return connection;
+		  
+	}
+    
+    
+	
+}
